bff02ccaf9bb186cad163efa62bda3f17b3ba812
ramin_yz <yazdani76ramin@gmail.com>
2022-08-25 11:45:54 +0430

    commit all | issue | problem in commiting so new folders and files created and edited

--- README.md
+++
@@ -1,2 +0,0 @@
-# HWK12_to_end
-all exercises from HWK12 to the end of the course 
---
+++ excersizes/ex2.py
@@ -0,0 +1,27 @@
+from ex1 import User as user_temp, User
+import json
+
+class User(user_temp):
+    def __init__(self,fullname,phone,email):
+        super(User, self).__init__(fullname, phone, email)
+    def to_json(self):
+        filename=self.fullname+".json"
+        with open(filename,"w") as f:
+            s= json.dumps(self.dict_instance())
+            f.write(s)
+
+    @classmethod
+    def from_json(cls,filename):
+        try:
+            with open(filename,"r") as f:
+                s = dict(json.load(f))
+                return User(*s.values())
+        except FileNotFoundError:
+            print(f"Could not find filename: {filename}")
+
+if __name__ == '__main__':
+
+    x = User("ramin","09124981090","yazdani@gmail.com")
+
+    y=User.from_json("ramin.json")
+    print(y.dict_instance())
